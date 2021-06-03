9d785cb5056f57e1bae148b01e3a8a33bf0b6df6
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-06-03 20:04:55 +0100

    🚧 Added gameplay system that spawn enemies
    Added the EnemySpawnSystem that spawns enemies when there are none left in the game world after a short delay.

---
+++ src/CodeTest.Game/Simulation/Systems/EnemySpawning/EnemySpawnerSystemFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeTest.Game
+{
+	public class EnemySpawnerSystemFactory : IWorldSystemFactory
+	{
+		private readonly EnemySpawnerConfiguration configuration;
+
+		public EnemySpawnerSystemFactory(EnemySpawnerConfiguration configuration)
+		{
+			this.configuration = configuration;
+		}
+
+		public IWorldSystem Build(World world)
+		{
+			return new EnemySpawnerSystem(world, configuration);
+		}
+	}
+}
