e54011f1e7fae131affb66a04c4539af65f32229
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-06-03 23:53:46 +0100

    🚧 First-pass approach to rendering the simulation in Unity

---
+++ src/CodeTestUnity/Assets/Scripts/WorldRenderer.cs
@@ -0,0 +1,33 @@
+﻿using CodeTest.Game.Simulation;
+using System.Collections.Specialized;
+using UnityEngine;
+
+namespace CodeTestUnity
+{
+	public class WorldRenderer : MonoBehaviour
+	{
+		[SerializeField] private SpriteRenderer background;
+		[SerializeField] private WorldGunRenderer gunRendererPrefab;
+		[SerializeField] private WorldEnemyRenderer enemyRendererPrefab;
+
+		public World World { get; private set; }
+
+		public void Render(World world)
+		{
+			World = world;
+
+			World.Guns.CollectionChanged += GunCollectionChanged;
+		}
+
+		private void GunCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+		{
+			foreach (object newItem in e.NewItems)
+			{
+				var worldGun = (WorldGun)newItem;
+				var gunRenderer = Instantiate(gunRendererPrefab);
+
+				gunRenderer.Render(worldGun);
+			}
+		}
+	}
+}
