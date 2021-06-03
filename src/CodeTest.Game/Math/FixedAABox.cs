29a9eb29acc6e9d62efe185eaba6e24a36f63ccc
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-06-03 20:01:54 +0100

    📦 Added a slim version of my deterministic maths library
    This code was NOT completed in the period of the code test

---
+++ src/CodeTest.Game/Math/FixedAABox.cs
@@ -0,0 +1,86 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CodeTest.Game.Math
+{
+	/// <summary>
+	/// Represents a <see cref="Fixed"/> axis-aligned box.
+	/// </summary>
+	public struct FixedAABox
+	{
+		private enum Side
+		{
+			Top,
+			Bottom,
+			Left,
+			Right
+		}
+
+		public FixedVector2 Center;
+
+		public FixedVector2 Extents;
+
+		public readonly FixedVector2 Max
+		{
+			get
+			{
+				return Center + Extents;
+			}
+		}
+
+		public readonly FixedVector2 Min
+		{
+			get
+			{
+				return Center - Extents;
+			}
+		}
+
+		public readonly Fixed Width
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get
+			{
+				return Extents.X * 2;
+			}
+		}
+
+		public readonly Fixed Height
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get
+			{
+				return Extents.Y * 2;
+			}
+		}
+
+		public FixedAABox(in FixedVector2 center, in FixedVector2 extents)
+		{
+			Center = center;
+			Extents = extents;
+		}
+
+		public readonly bool Contains(in FixedVector2 point)
+		{
+			var min = Min;
+			var max = Max;
+
+			return min.X <= point.X
+				&& max.X >= point.X
+				&& min.Y <= point.Y
+				&& max.Y >= point.Y;
+		}
+
+		public readonly bool Overlaps(in FixedAABox bounds)
+		{
+			var min = Min;
+			var max = Max;
+			var boundsMin = bounds.Min;
+			var boundsMax = bounds.Max;
+
+			return min.X <= boundsMax.X
+				&& max.X >= boundsMin.X
+				&& min.Y <= boundsMax.Y
+				&& max.Y >= boundsMin.Y;
+		}
+	}
+}
---
+++ src/CodeTest.Game/Math/Internal/MathEngine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace CodeTest.Game.Math.Internal
+{
+	internal static class MathEngine
+	{
+		internal static readonly long[] sin;
+		internal static readonly long[] tan;
+		internal static readonly long[] asin;
+		internal static readonly long[] acos;
+		internal static readonly long[] atan;
+		internal static readonly long[] sqrt;
+
+		static MathEngine()
+		{
+			sin = Load ("CodeTest.Game.Math.LUT.Sin.bin");
+			tan = Load ("CodeTest.Game.Math.LUT.Tan.bin");
+			asin = Load("CodeTest.Game.Math.LUT.Asin.bin");
+			acos = Load("CodeTest.Game.Math.LUT.Acos.bin");
+			atan = Load("CodeTest.Game.Math.LUT.Atan.bin");
+			sqrt = Load("CodeTest.Game.Math.LUT.Sqrt.bin");
+		}
+
+		private static long[] Load(string resourceName)
+		{
+			var assembly = typeof(MathEngine).Assembly;
+			var stream = assembly.GetManifestResourceStream(resourceName);
+
+			byte[] data = new byte[stream.Length];
+			stream.CopyTo(new MemoryStream(data));
+
+			long[] copiedData = new long[stream.Length / 8];
+			Buffer.BlockCopy(data, 0, copiedData, 0, data.Length);
+
+			return copiedData;
+		}
+	}
+}
