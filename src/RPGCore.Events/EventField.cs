0942e44e3e73aef66806736eb36737a8117ed974
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-06-04 00:00:47 +0100

    📦 Add a copy of RPGCore.Events

---
+++ src/RPGCore.Events/EventField.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace RPGCore.Events
+{
+	public sealed class EventField<T> : IEventField<T>
+	{
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		private T internalValue;
+
+		[JsonIgnore]
+		public EventFieldHandlerCollection Handlers { get; }
+
+		public T Value
+		{
+			get => internalValue;
+			set
+			{
+				Handlers.InvokeBeforeChanged();
+				internalValue = value;
+				Handlers.InvokeAfterChanged();
+			}
+		}
+
+		public EventField()
+		{
+			Handlers = new EventFieldHandlerCollection(this);
+		}
+
+		public EventField(T value)
+			: this()
+		{
+			internalValue = value;
+		}
+	}
+}
---
+++ src/RPGCore.Events/EventFieldMirrorHandler.cs
@@ -0,0 +1,23 @@
+namespace RPGCore.Events
+{
+	public sealed class EventFieldMirrorHandler<T> : IEventFieldHandler
+	{
+		public IReadOnlyEventField<T> SourceField { get; }
+		public IEventField<T> Target { get; }
+
+		public EventFieldMirrorHandler(IReadOnlyEventField<T> source, IEventField<T> target)
+		{
+			SourceField = source;
+			Target = target;
+		}
+
+		public void OnBeforeChanged()
+		{
+		}
+
+		public void OnAfterChanged()
+		{
+			Target.Value = SourceField.Value;
+		}
+	}
+}
